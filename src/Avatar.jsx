699f0426e49660c6d59b502baffd62e6e2bba5db
GonçaloDiogoCoelho <goncalodmvm.c@gmail.com>
2022-10-11 16:08:13 +0100

    workshop simpsons react

---
+++ src/Avatar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+const lastName = "SIMPSON"
+
+const avatar = [{FirstName: "Homer", lastName, avatar:"" }, {FirstName: "Bart", lastName, avatar:"" }]
+
+const Avatar = ({name, avatar}) => {
+  return (
+    <div>
+        <img src="#" />
+        <h3>{FirstName}</h3>
+    </div>
+    )
+}
+
+export default Avatar
